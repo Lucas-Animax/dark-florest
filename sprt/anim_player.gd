5ee26b1476534873c4cc45dd9ca057d34c9a4c95
Lucas-Animax <lucaslemos1402@gmail.com>
2024-07-01 19:23:02 -0300

    moving

---
+++ sprt/anim_player.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+class_name Anim_Player
+@export_category("Objects")
+@export var anim:AnimationPlayer
+func _animate(dic:Vector2):
+	if dic.x != 0:
+		_anim_horizontal(dic)
+	_check_dic(dic)
+	pass
+func _anim_horizontal(dic):
+	if dic.x != 0:
+		anim.play("walk")
+	anim.play("idle")
+	pass
+func _check_dic(dic):
+	if dic.x > 0:
+		flip_h = false
+	elif dic.x < 0:
+		flip_h = true
+	
+	
+	
+	
+	pass
