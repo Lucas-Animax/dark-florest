adc9df38ccb17b72542e9b8c1ad212274e8822c9
Lucas-Animax <lucaslemos1402@gmail.com>
2024-07-02 17:15:12 -0300

    up-func

--- sprt/anim_player.gd
+++ sprt/anim_player.gd
@@ -1,23 +1,16 @@
 extends Sprite2D
 class_name Anim_Player
+var on_floor:bool = false
 @export_category("Objects")
 @export var anim:AnimationPlayer
 func _animate(dic:Vector2):
-	if dic.y != 1:
+	if dic.y != 0:
 		_anim_vertical(dic)
-	elif  dic.x != 0:
-		_anim_horizontal(dic)
-		
+	else: _anim_horizontal(dic)
 	_check_dic(dic)
 	pass
-func _anim_horizontal(dic):
-	if dic.x != 0:
-		anim.play("walk")
-		return
-	anim.play("idle")
-	pass
 func _check_dic(dic):
-	if dic.x > 0:
+	if dic.x > 1:
 		flip_h = false
 	elif dic.x < 0:
 		flip_h = true
@@ -27,10 +20,16 @@ func _check_dic(dic):
 	
 	pass
 func _anim_vertical(dic):
-	if dic.y > 1:
+	if dic.y > 0:
 		anim.play("down")
-	if dic.y < 1:
+	elif dic.y < 0:
 		anim.play("up")
 	
 	
+	pass
+func _anim_horizontal(dic):
+	if dic.x != 0:
+		anim.play("walk")
+		return
+	anim.play("idle")
 	pass
--- sprt/player.gd
+++ sprt/player.gd
@@ -3,6 +3,8 @@ class_name Player
 #region int
 var jump_cont:int = 2
 #endregion
+#region bool
+#endregion
 @export_category("Objects")
 @export var spr:Sprite2D
 @export_category("Variables")
@@ -14,29 +16,25 @@ func _physics_process(delta:float):
 	move_and_slide()
 	_move_horizontal()
 	_move_vertical()
-	_gravit()
 	spr._animate(velocity)
-	
 	pass
 func _move_horizontal():
 	var dic:float = Input.get_axis("move_left","move_right")
 	velocity.x = dic * speed
 	pass
-func _gravit():
-	velocity.y += gravit_value
-	if velocity.y > gravit_value:
-		velocity.y = gravit_value
-	
-	pass
 func _move_vertical():
 	if is_on_floor():
 		jump_cont = 0
-		
-	if Input.is_action_just_pressed("jump") and jump_cont < 2:
+	if Input.is_action_just_pressed("jump") and jump_cont < 1 and is_on_floor():
+		velocity.y -= jump_value
+		jump_cont += 1
+	elif Input.is_action_just_pressed("jump") and jump_cont == 1 and not is_on_floor():
 		velocity.y -= jump_value
 		jump_cont += 1
 		return
-	print(jump_cont)
-	
+	if not is_on_floor():
+		velocity.y += gravit_value 
+		if velocity.y > gravit_value:
+			velocity.y = gravit_value
 	
 	pass
